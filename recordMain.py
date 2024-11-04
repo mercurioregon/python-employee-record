3419c67409c23bc2c404614c31329a5e624985c5
mercurioregon <jamesm620@gmail.com>
2024-11-04 10:01:20 -0800

    add function

--- recordMain.py
+++ recordMain.py
@@ -11,3 +11,31 @@ employee = cursor.fetchone()
 cursor.close()
 
 return employee is not None
+
+def add_employee():
+    Id = input("Enter employee ID: ")
+    if check_employee(Id):
+        print("Employee already exists. Please try again.")
+        return 
+    
+    else:
+        Name = input ("Enter employee name: ")
+        Position= input("Enter employee position: ")
+        Salary = input ("Enter employee salary: ")
+        
+        sql = "INSERT INTO  employees (id, name, position, salary) VALUES (%s, %s, %s, %s)"
+        data = (Id, Name, Position, Salary)
+        
+        try: 
+            cursor.execute(sql,data)
+            
+            con.commit()
+            print("Employee added successfully.")
+            
+        except mysql.connector.Error as err:
+            print(f"Error : {err}")
+            con. rollback()
+            
+        finally:
+            cursor.close()
+
