dff5033093f16bce37fdf76bead2566a1d322e60
mercurioregon <jamesm620@gmail.com>
2024-11-06 11:57:45 -0800

    gui project

---
+++ GUI.py
@@ -0,0 +1,10 @@
+from tkinter import  *
+
+root = Tk()
+root.title("Welcome to my Visual Display")
+root.geometry('350x200')
+root.mainloop()
+lbl = Label(root, text = "Your name is Jim, right?")
+lbl.grid()
+root.mainloop()
+
