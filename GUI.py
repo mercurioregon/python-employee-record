198444c3fd6bf1d5388e33a1f1d8a7735381d961
mercurioregon <jamesm620@gmail.com>
2024-11-06 12:11:33 -0800

    gui TEXT FIELD

--- GUI.py
+++ GUI.py
@@ -1,10 +1,22 @@
-from tkinter import  *
+rom tkinter import  *
 
 root = Tk()
 root.title("Welcome to my Visual Display")
 root.geometry('350x200')
-root.mainloop()
+
 lbl = Label(root, text = "Your name is Jim, right?")
 lbl.grid()
-root.mainloop()
 
+txt = Entry(root, width=10)
+txt.grid(column =1, row=0)
+
+#INCLUDES BUTTON RESPONSE AND TEXT FIELD
+def clicked():
+    res = "You wrote:" + txt.get()
+    # lbl.configure(text = "I was just, like, clicked and stuff.")
+    lbl.configure(text = res)
+btn = Button(root, text = "Click me, man." ,
+                           fg = "red",  command =clicked)
+btn.grid(column=2, row=0)
+
+root.mainloop()
