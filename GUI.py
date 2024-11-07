f83609d5ad9789acba50d69289bf0d6209198bfb
mercurioregon <jamesm620@gmail.com>
2024-11-07 13:50:20 -0800

    menu

--- GUI.py
+++ GUI.py
@@ -1,8 +1,16 @@
-rom tkinter import  *
+from tkinter import  *
 
 root = Tk()
 root.title("Welcome to my Visual Display")
 root.geometry('350x200')
+
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label="New")
+menu.add_cascade(label="File", menu=item,)
+menu.add_cascade(label="Free Chicken", menu=item)
+root.config(menu=menu)
+
 
 lbl = Label(root, text = "Your name is Jim, right?")
 lbl.grid()
@@ -10,7 +18,7 @@ lbl.grid()
 txt = Entry(root, width=10)
 txt.grid(column =1, row=0)
 
-#INCLUDES BUTTON RESPONSE AND TEXT FIELD
+
 def clicked():
     res = "You wrote:" + txt.get()
     # lbl.configure(text = "I was just, like, clicked and stuff.")
