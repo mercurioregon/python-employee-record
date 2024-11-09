8bd1b506e8ebb9c38a26675f9f2199288419d526
mercurioregon <jamesm620@gmail.com>
2024-11-09 14:21:17 -0800

    calculator code

--- calculator.py
+++ calculator.py
@@ -3,3 +3,11 @@ import sys
 from PyQt6.QtWidgets import QApplication, Qlabel, QWidget
 
 app = QApplication([])
+
+window + QWidget()
+window.setwindowtitle("PyQt App")
+window.setGeometry(100, 100, 280, 80)
+helloMsg = QLabel("<h1>What's up?</h1>", parent = window)
+helloMsg.move(60, 15)
+window.show()
+sys.exit(app.exec())
