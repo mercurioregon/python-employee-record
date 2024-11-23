c0f0aab913c0585a8e1534da6bb1b84ef3694d47
mercurioregon <jamesm620@gmail.com>
2024-11-23 15:55:00 -0800

    hangman contd

--- hangman.py
+++ hangman.py
@@ -12,3 +12,19 @@ if _name_ =='_main_':
     for i in word:
         print('_', end=' ')
     print()
+
+    playing = True
+    letterguessed = ''
+    chances = len(word) + 2
+    correct = 0
+    flag = 0
+    try:
+        while (chances != 0) and flag == 0:
+            print()
+            chances -= 1
+
+            try:
+                guess = str(input('Enter a letter '))
+            except: 
+                print('Enter only a letter.  This is Hangman. What are you doing?')
+                continue    
