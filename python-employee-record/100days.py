8cc11cb7dd8bca57ac4a0e77f51de7686b1ac002
mercurioregon <jamesm620@gmail.com>
2024-11-26 13:37:33 -0800

    100 days course

---
+++ python-employee-record/100days.py
@@ -0,0 +1,15 @@
+
+#More string stuff
+
+#inpit gies into print.  cleaner code
+# print("Hello " + input("What is your name, slick?") + "!")
+
+# print(len(input("What is your name?")))
+
+# username = input("Wha is your name?")
+
+print("This is a band name generator.")
+city = input("What is the name of the city you grew up in?\n")
+food = input("What is one of your favorite foods?\n")
+print("Your band name could be " + food + " " + city + ".")
+
