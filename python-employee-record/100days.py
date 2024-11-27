e91d8153373eacee88d53fc8753e950bb7eceb31
mercurioregon <jamesm620@gmail.com>
2024-11-27 14:10:26 -0800

    TIP CALC

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -13,3 +13,14 @@ city = input("What is the name of the city you grew up in?\n")
 food = input("What is one of your favorite foods?\n")
 print("Your band name could be " + food + " " + city + ".")
 
+# TIP CALCULATOR
+print("Tip calculator")
+billTotal = float(input("What was the total bill?"))
+print(billTotal)
+tipAmount = int(input("How much tip would you like to give?"))
+print(tipAmount)
+peopleTotal = int(input("How many people will split the bill?"))
+print(peopleTotal)
+grandTotal = (tipAmount /100 * billTotal + billTotal) / peopleTotal
+print(f"Each person should pay: {grandTotal}")
+
