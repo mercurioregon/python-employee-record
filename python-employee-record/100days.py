7ade65879c6717bd6d3893a270b7511b16678d27
mercurioregon <jamesm620@gmail.com>
2024-12-10 16:28:13 -0800

    quiz game 3rd pg

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -254,3 +254,13 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+#questionbrain page
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def  next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Q:{self.question_number}; {current_question.text}(True or False?): ")
