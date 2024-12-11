e8e7c7de7387a85fb5c7e06fe7164d953315802e
mercurioregon <jamesm620@gmail.com>
2024-12-10 16:15:22 -0800

    quiz game2

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -242,5 +242,15 @@ class Question:
 new_q = Question("Bacon?", "False")
 print(new_q.text)
 
+#main
 
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
