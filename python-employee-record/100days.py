12aab2780d7461ffe2782193b296ba48a645f2b2
mercurioregon <jamesm620@gmail.com>
2024-12-26 15:33:05 -0800

    snake game 2

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -524,6 +524,7 @@ screen.exitonclick()
 
 from turtle import Screen, Turtle
 import time
+from turtledemo.penrose import start
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -547,10 +548,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(.1)
-    for seg in segments:
-        seg.forward(20)
 
-
+    for seg_number in range(len(segments) -1, 0,-1):
+       new_x =  segments[seg_number - 1].xcor()
+       new_y =  segments[seg_number - 1].ycor()
+       segments[seg_number].goto(new_x, new_y)
+    segments[0].forward(20)
 
 
 screen.exitonclick()
