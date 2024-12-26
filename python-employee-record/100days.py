8ba78968d470a6dc9ac4593785e2ecc16b71d8c2
mercurioregon <jamesm620@gmail.com>
2024-12-26 15:51:09 -0800

    snake game 3

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -557,3 +557,33 @@ while game_is_on:
 
 
 screen.exitonclick()
+
+#snake.py is now:
+
+from turtle import Turtle
+START_POSITIONS = [(0,0), (-20, 0), (-40,0)]
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.move()
+
+    def create_snake(self):
+        for position in START_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color ("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+            for seg_number in range(len(self.segments) - 1, 0, -1):
+                new_x = self.segments[seg_number - 1].xcor()
+                new_y = self.segments[seg_number - 1].ycor()
+                self.segments[seg_number].goto(new_x, new_y)
+                self.segments[0].forward(20)
+
+
+
