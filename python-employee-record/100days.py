32146322d9c2c0fde9b9c8c355402b9ec9e6446e
mercurioregon <jamesm620@gmail.com>
2024-12-26 14:58:31 -0800

    snake game 1

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -519,3 +519,21 @@ screen.exitonclick()
 
 
 screen.exitonclick()
+
+#snake game
+
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Ye Olde Snakee Gamee")
+
+start_positions = [(0,0), (-20, 0), (-40,0)]
+
+for position in start_positions:
+    new_segment = Turtle("square")
+    new_segment.color ("white")
+    new_segment.goto(position)
+
+screen.exitonclick()
