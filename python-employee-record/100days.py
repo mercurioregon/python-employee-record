e18cd750f5a762f7db0c35c87a6bc0a38536fa29
mercurioregon <jamesm620@gmail.com>
2024-12-30 16:04:40 -0800

    snake game 6

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -607,4 +607,24 @@ class Snake:
 
     def right(self):
                 if self.head.heading() != LEFT:
-                        self.head.setheading(RIGHT)
+                    
+
+#food.py -
+
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=+.5, stretch_wid=.5)
+        self.color("green")
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
