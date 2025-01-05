3bf89b3a826ab0421edd4baa2977381774ecb86f
mercurioregon <jamesm620@gmail.com>
2025-01-05 14:45:40 -0800

    bounce

--- python-employee-record/100days.py
+++ python-employee-record/100days.py
@@ -1283,7 +1283,7 @@ class Paddle(Turtle):
 
 #ball.py
 
-    from turtle import  Turtle
+from turtle import  Turtle
 
 class Ball(Turtle):
 
@@ -1292,11 +1292,19 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() +10
-        new_y = self.ycor() +10
+        new_x = self.xcor() +self.x_move
+        new_y = self.ycor() +self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
+
+
+
 
 
 
